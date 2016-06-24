4bd4ed3a5e71080fecc9aef55b94f9132dc08c44
Krystyna Singleton <ksingleton88@gmail.com>
2016-06-23 17:56:38 -0700

    Initial Commit

--- js/run.js
+++ js/run.js
@@ -1,12 +1,12 @@
-
-var userTurn;
-
 $(document).ready(function(){
+	
+	var gameInProgress = true;
 
 	restart();
 
 	function restart(){
 		$('.x, .o').removeClass('x o');
+		gameInProgress = true;
 	}
 
 	$('.newGame').on('click', function(){
@@ -14,31 +14,41 @@ $(document).ready(function(){
 	})
 
 	$('.square').on('click', function(){
-		var className = userTurn ? 'x' : 'o';
-		//same as: if(userTurn){className='x;} else{className='o'}
-		userTurn =! userTurn //tells us whose turn it is (toggles)
+		if (!gameInProgress) {
+			return;
+		}
+		
 		$(this).addClass('x');
 
+		setTimeout(testSolutions); 
 		setTimeout(computer, 500);
-		testSolutions();
-
 	})
 
 	function computer(){
+		if (!gameInProgress) {
+			return;
+		}
+		
 		var availableSpaces = $('.square').not('.x, .o');
 		var number = Math.floor(Math.random() * availableSpaces.length);
 
 		availableSpaces.eq(number).addClass('o');
+		
+		setTimeout(testSolutions, 50);
 	}
 
 
 	var solutions = [
+		// rows
 		[0, 1, 2],
 		[3, 4, 5],
 		[6, 7, 8],
+		// diag
 	 	[2, 4, 6],
 		[0, 4, 8],
+		// cols
 		[0, 3, 6],
+		[1, 4, 7],
 		[2, 5, 8],
 	];
 
@@ -46,42 +56,35 @@ $(document).ready(function(){
 	var $cells = $('.square');
 
 	function testSolutions(){
-	
+		var solved = false;
 		
 		// Goes through each solution
 		solutions.forEach(function(solution) {
-			if (checkSolution(solution)) {
+			solved = checkSolution(solution);
+			if (solved) {
 				//winner
-				alert('test');
-	  		}
+				alert(solved + ' is the winner!');
+				gameInProgress = false;
+				return;
+	  	}
 		})
 	}
 
 	function checkSolution(map) {
-	  var solved = true;
-	  var firstCellId = map.shift();
-	  var matcher;
+		var $solutionCells = $cells.filter(function(index) {
+			return map.indexOf(index) != -1;
+		});
+
+		if ($solutionCells.filter('.x').length === 3) {
+			console.log('checkSolution', 'XXX', map);
+			return 'X';
+		}
+		if ($solutionCells.filter('.o').length === 3) {
+			console.log('checkSolution', 'OOO', map);
+			return 'O';
+		}
 	  
-	  if ($cells.eq(firstCellId).hasClass('x')) {
-	    matcher = 'x';
-	  }
-	  else if ($cells.eq(firstCellId).hasClass('o')) {
-	    matcher = 'o';
-	  }
-	  if (!matcher) {
-	    return false;
-	  }
-	  
-	  // [4, 5]
-	  map.forEach(function(cellId) {
-	    if (!$cells.eq(cellId).hasClass(matcher)) {
-	      return false;
-	    }
-	  });
-	  
-	  return solved;
+	  return false;
 	}
 
 });
-
-
